45dc497dba43cd8e4c15bccb7c473057749fd1b0
Vitaliy Zarubin <keygenqt@gmail.com>
2021-11-05 01:32:05 +0300

    Added ci

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,22 +11,25 @@ version = "1.0"
 kotlin {
     android()
 
-    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget = when {
-        System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
-        System.getenv("NATIVE_ARCH")?.startsWith("arm") == true -> ::iosSimulatorArm64
-        else -> ::iosX64
+    System.getenv("GITHUB_REPOSITORY") ?: run {
+
+        val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget = when {
+            System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
+            System.getenv("NATIVE_ARCH")?.startsWith("arm") == true -> ::iosSimulatorArm64
+            else -> ::iosX64
+        }
+
+        iosTarget("ios") {}
+
+        cocoapods {
+            summary = "Some description for the Shared Module"
+            homepage = "Link to the Shared Module homepage"
+            ios.deploymentTarget = "14.1"
+            frameworkName = "shared"
+            podfile = project.file("../iosApp/Podfile")
+        }
     }
 
-    iosTarget("ios") {}
-
-    cocoapods {
-        summary = "Some description for the Shared Module"
-        homepage = "Link to the Shared Module homepage"
-        ios.deploymentTarget = "14.1"
-        frameworkName = "shared"
-        podfile = project.file("../iosApp/Podfile")
-    }
-    
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
