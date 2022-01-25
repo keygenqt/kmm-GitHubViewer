d2c3aa4baea839258e2f0a6a128f55e05c53f8b3
Vitaliy Zarubin <keygenqt@gmail.com>
2022-01-26 00:17:20 +0300

    Update form

--- iosApp/GitHubViewer/guest/ui/signIn/SignInScreen.swift
+++ iosApp/GitHubViewer/guest/ui/signIn/SignInScreen.swift
@@ -8,6 +8,50 @@
 import AlertToast
 import SwiftUI
 
+public extension View {
+    @inlinable func validate(of: String, error: @escaping (_ error: String?) -> Void) -> some View {
+        onChange(of: of, perform: { value in
+            error(nil)
+            if value.contains("0") {
+                error("Nickname contains numbers")
+            } else if value.count > 10 {
+                error("Nickname too long")
+            }
+        })
+    }
+}
+
+struct AppTextField: View {
+    var clickError: (_ error: String?) -> Void
+
+    @State private var username: String = ""
+    @State private var usernameError: String?
+
+    var body: some View {
+        ZStack {
+            TextField(text: $username, prompt: Text(L10nSignIn.formNickname)) {
+                Text(L10nSignIn.formNickname)
+            }
+            .validate(of: username, error: { value in
+                usernameError = value
+            })
+            .keyboardType(.asciiCapable)
+            .padding(.trailing, usernameError != nil ? 30 : 0)
+
+            HStack {
+                Spacer()
+                if usernameError != nil {
+                    Image(systemName: "exclamationmark.circle.fill")
+                        .foregroundColor(.error)
+                        .onTapGesture(count: 1) {
+                            clickError(usernameError)
+                        }
+                }
+            }
+        }
+    }
+}
+
 struct SignInScreen: View {
     // model
     @ObservedObject var viewModel = SignInViewModel()
@@ -17,41 +61,20 @@ struct SignInScreen: View {
     @EnvironmentObject var router: RouterGuest
     // form states
     @State private var username: String = ""
-    @State private var isError = false
     @State private var usernameError: String?
+    @State private var isShowError: Bool = false
     // page values
     @Environment(\.openURL) var openURL
-    @State private var showToast = false
 
     var body: some View {
         Form {
             Section {
-                ZStack {
-                    TextField(text: $username, prompt: Text(L10nSignIn.formNickname)) {
-                        Text(L10nSignIn.formNickname)
+                AppTextField(clickError: { error in
+                    usernameError = error
+                    if error != nil {
+                        isShowError = true
                     }
-                    .keyboardType(.asciiCapable)
-                    .onChange(of: username, perform: { value in
-                        usernameError = nil
-                        if value.matches("[0-9]") {
-                            usernameError = "Nickname contains numbers"
-                        } else if value.count > 10 {
-                            usernameError = "Nickname too long"
-                        }
-                    })
-                    .padding(.trailing, usernameError != nil ? 30 : 0)
-
-                    HStack {
-                        Spacer()
-                        if usernameError != nil {
-                            Image(systemName: "exclamationmark.circle.fill")
-                                .foregroundColor(.error)
-                                .onTapGesture(count: 1) {
-                                    showToast = true
-                                }
-                        }
-                    }
-                }
+                })
             }
             Section {
                 Button(L10nSignIn.formButtonSubmit) {
@@ -65,7 +88,7 @@ struct SignInScreen: View {
         }
         .navigationBarTitle(L10nSignIn.title)
         .navigationBarTitleDisplayMode(.inline)
-        .toast(isPresenting: $showToast) {
+        .toast(isPresenting: $isShowError) {
             AlertToast(
                 displayMode: .banner(.pop),
                 type: .systemImage("exclamationmark.circle.fill", .error),
