2682053c34d2508291114fe52ce163c9da5cd310
Vitaliy Zarubin <keygenqt@gmail.com>
2022-01-25 10:29:52 +0300

    Update form

--- iosApp/GitHubViewer/guest/ui/signIn/SignInScreen.swift
+++ iosApp/GitHubViewer/guest/ui/signIn/SignInScreen.swift
@@ -5,6 +5,7 @@
 //  Created by Виталий Зарубин on 13.01.2022.
 //
 
+import AlertToast
 import SwiftUI
 
 struct SignInScreen: View {
@@ -14,28 +15,70 @@ struct SignInScreen: View {
     @EnvironmentObject var graph: GraphObservable
     // router
     @EnvironmentObject var router: RouterGuest
-    // page values
+    // form states
     @State private var username: String = ""
+    @State private var isError = false
+    @State private var usernameError: String?
+    // page values
+    @Environment(\.openURL) var openURL
+    @State private var showToast = false
 
     var body: some View {
         Form {
             Section {
-                TextField(text: $username, prompt: Text(L10nSignIn.formNickname)) {
-                    Text(L10nSignIn.formNickname)
+                ZStack {
+                    TextField(text: $username, prompt: Text(L10nSignIn.formNickname)) {
+                        Text(L10nSignIn.formNickname)
+                    }
+                    .keyboardType(.asciiCapable)
+                    .onChange(of: username, perform: { value in
+                        usernameError = nil
+                        if value.matches("[0-9]") {
+                            usernameError = "Nickname contains numbers"
+                        } else if value.count > 10 {
+                            usernameError = "Nickname too long"
+                        }
+                    })
+                    .padding(.trailing, usernameError != nil ? 30 : 0)
+
+                    HStack {
+                        Spacer()
+                        if usernameError != nil {
+                            Image(systemName: "exclamationmark.circle.fill")
+                                .foregroundColor(.error)
+                                .onTapGesture(count: 1) {
+                                    showToast = true
+                                }
+                        }
+                    }
                 }
-                .keyboardType(.asciiCapable)
             }
             Section {
-                Link(L10nSignIn.formButtonSubmit,
-                     destination: URL(string: "https://www.apple.com")!)
-                    .buttonStyle(BottomPrimaryStyle())
-                    .listRowInsets(.init())
-                    .listRowBackground(Color.clear)
+                Button(L10nSignIn.formButtonSubmit) {
+                    openURL(URL(string: "https://www.apple.com")!)
+                }
+                .buttonStyle(BottomPrimaryStyle())
+                .listRowInsets(.init())
+                .listRowBackground(Color.clear)
             }
             .listRowBackground(Color.clear)
         }
         .navigationBarTitle(L10nSignIn.title)
         .navigationBarTitleDisplayMode(.inline)
+        .toast(isPresenting: $showToast) {
+            AlertToast(
+                displayMode: .banner(.pop),
+                type: .systemImage("exclamationmark.circle.fill", .error),
+                title: usernameError ?? "Error validation",
+                style: .style(
+                    backgroundColor: .errorContainer,
+                    titleColor: .onErrorContainer,
+                    subTitleColor: .onErrorContainer,
+                    titleFont: Font.custom(PoppinsName(.Medium), size: 16),
+                    subTitleFont: Font.custom(PoppinsName(.Medium), size: 12)
+                )
+            )
+        }
     }
 }
 
--- iosApp/Utility/Extension/StringExt.swift
+++ iosApp/Utility/Extension/StringExt.swift
@@ -10,4 +10,8 @@ import SwiftUI
 /**
  * Genearte AttributedString for texts app
  */
-extension String {}
+extension String {
+    func matches(_ regex: String) -> Bool {
+        return range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+}
