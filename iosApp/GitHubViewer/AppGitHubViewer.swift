da707c2d5b5b056eccf7ae37061ca6276302bb44
Vitaliy Zarubin <keygenqt@gmail.com>
2022-02-07 21:04:03 +0300

    Add ios Firebase

--- iosApp/GitHubViewer/AppGitHubViewer.swift
+++ iosApp/GitHubViewer/AppGitHubViewer.swift
@@ -5,13 +5,23 @@
 //  Created by Виталий Зарубин on 05.10.2021.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct AppGitHubViewer: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
     var body: some Scene {
         WindowGroup {
             NavGraph()
+        }
+    }
+
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+            FirebaseApp.configure()
+            return true
         }
     }
 }
